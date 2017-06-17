724b6ada2d73d29681804b0d2d4d18c97402f1b2
Szűcs Tibor <job.szucstibor@gmail.com>
2017-06-17 18:35:06 +0200

    Ordering, export inventory

--- gameInventory.py
+++ gameInventory.py
@@ -3,27 +3,29 @@
 import operator
 import csv
 import sys
-_inventory = {"fucks given": 0, "shit": 1, "stack": 1}
+import os
+_inventory = {"fucks given": 0, "shit": 2, "stack": 3}
 addedItems = []
 total = 0
 imported_inventory = []
+exported_inventory = []
 i = 0
+ordering = ""
+keys = []
+values = []
+
 # Displays the inventory.
-
-
-def decor(func):
-    def wrap():
-        print("==========================")
-        func()
-        print("==========================")
-    return wrap
 
 
 def displayInventory(inventory):
     total = 0
+    print("Inventory: ")
+    print("Count   Item name")
+    print("------------------------------")
     for k, v in _inventory.items():
-        print('{:>0}  {:>12}'.format(v, k))
+        print('{:>2}  {:>12}'.format(v, k))
         total = total + v
+    print("------------------------------")
     print("Total number of items: ", total)
 
 # Adds to the inventory dictionary a list of items from added_items.
@@ -45,20 +47,22 @@ def add_to_inventory(inventory, addedItems):
 
 
 def print_table(inventory, order=None):
-    if order is None:
-        displayInventory(inventory)
-    elif order is "count,asc":
-        sorted(_inventory.items(), key=operator.itemgetter(1))
-        displayInventory(inventory)
-    elif order is "count,desc":
-        sorted(_inventory.items(), key=operator.itemgetter(1), reverse=True)
-        displayInventory(inventory)
+    displayInventory(_inventory)
 
 
+def print_table(inventory, order=True):
+    sorted(_inventory, key=_inventory.__getitem__)
+    displayInventory(_inventory)
+
+
+def print_table(inventory, order=False):
+    sorted(_inventory, key=_inventory.__getitem__, reverse=True)
+    displayInventory(_inventory)
 # Imports new inventory items from a file
 # The filename comes as an argument, but by default it's
 # "import_inventory.csv". The import automatically merges items by name.
 # The file format is plain text with comma separated values (CSV).
+
 
 def import_inventory(inventory, filename="import_inventory.csv"):
     with open(sys.argv[1], 'r') as csvfile:
@@ -73,10 +77,18 @@ def import_inventory(inventory, filename="import_inventory.csv"):
 
 
 def export_inventory(inventory, filename="export_inventory.csv"):
-    with open("export_inventory.csv", 'rb') as _inventory:
-        wr = csv.writer(_inventory)
-        wr.writerow(_inventory)
+    export = open("export_inventory.csv", "w")
+    export.writerow(_inventory)
+    export.close()
 
 import_inventory(_inventory, filename="import_inventory.csv")
-decorated = decor(print_table(_inventory, order=None))
-decorated()
+displayInventory(_inventory)
+ordering = (input("How would you like to organise the inventory? : "))
+
+
+if ordering == "":
+    print_table(_inventory, order=None)
+elif ordering == "count,asc":
+    print_table(_inventory, order=True)
+elif ordering == "count,desc":
+    print_table(_inventory, order=False)
